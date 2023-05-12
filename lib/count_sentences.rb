c5da3b94debf3207480ffcc6c3c5d8e9d819815f
teklumezgebo <mezge002@umn.edu>
2023-05-11 21:31:53 -0500

    commit tree da1794a47249122bda051f8af5e6ccd6e1e3818c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?("!", ".", "?")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    end_of_sentence = self.split
+    number_of_sentences = []
+    end_of_sentence.each do |word|
+      if word.end_with?(".", "?", "!")
+        number_of_sentences << word
+      end
+    end
+
+    number_of_sentences.count
 
   end
 end
